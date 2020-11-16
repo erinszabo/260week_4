af2d3169e5de0a08276e63e99d372dad426c42b7
erinszabo <55733370+erinszabo@users.noreply.github.com>
2020-11-15 18:58:21 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+chap 4 exercises: 1,2,3,5,and 10 or 11
+"""
+
+
+if __name__ == '__main__':
+    print()
+    print("1) Modify the infix-to-postfix "
+          "algorithm so that it can handle errors.")
+
+    print()
+    print("2) Modify the postfix evaluation algorithm so that it can handle errors.")
+
+    print()
+    print("3) Implement a direct infix evaluator that combines the functionality of infix-to-postfix "
+          "conversion and the postfix evaluation algorithm. Your evaluator should process infix tokens "
+          "from left to right and use two stacks, one for operators and one for operands, to perform the evaluation.")
+
+    print()
+    print("5) Implement the Queue ADT, using a list such that the rear of the queue is at the end of the list.")
+
+    print()
+    print("...10 or 11 here...")
+    print()
+    print()
